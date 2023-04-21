cadbe29d435ca171568dd637e2c2067ff4bfd235
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-04-21 20:05:52 +0530

    Create valid__palindrome.java

---
+++ valid__palindrome.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i<s.length(); i++){
+            if(Character.isDigit(s.charAt(i)) || Character.isLetter(s.charAt(i))){
+                sb.append(s.charAt(i));
+            }
+        }
+        int n = sb.length() - 1;
+
+        for(int i = 0; i < (sb.length()/2); i++  ){
+            if(sb.charAt(i) != sb.charAt(n-i)){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
+//https://leetcode.com/problems/valid-palindrome/description/
