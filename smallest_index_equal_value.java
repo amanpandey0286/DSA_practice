8833b29a004b8a8c2cd99112b1d252bd8a617763
Aman Pandey <80692326+amanpandey0286@users.noreply.github.com>
2023-10-21 19:34:08 +0530

    Create smallest_index_equal_value.java

---
+++ smallest_index_equal_value.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int smallestEqual(int[] nums) {
+        int ans = -1;
+        for(int i = 0; i < nums.length; i++){
+            if((i%10) == nums[i]){
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
+
+//https://leetcode.com/problems/smallest-index-with-equal-value/
